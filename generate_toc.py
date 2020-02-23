9aa1961d36d33154963265f7bb00ce25c58adaa6
TheBojda <thebojda@gmail.com>
2020-02-23 17:16:39 +0100

    New posts (2020-02-23) added subtitles to toc

--- generate_toc.py
+++ generate_toc.py
@@ -4,22 +4,37 @@ from html.parser import HTMLParser
 
 class TitleExtractor(HTMLParser):
     inHeading = False
+    inSubHeading = False
     title = ''
+    subtitle = ''
 
     def handle_starttag(self, tag, attrs):
         if tag == 'h1':
             self.inHeading = True
+        if tag == 'section':
+            for name, value in attrs:
+                if name == 'data-field' and value == 'subtitle':
+                    self.inSubHeading = True
 
     def handle_data(self, data):
         if self.inHeading:
             self.title = data
+        if self.inSubHeading:
+            self.subtitle = data
 
     def handle_endtag(self, tag):
         if tag == 'h1':
             self.inHeading = False
+        if tag == 'section':
+            self.inSubHeading = False
 
     def get_title(self):
         return self.title
+
+    def get_subtitle(self):
+        if len(self.subtitle) > 100:
+            return ''
+        return self.subtitle
 
 
 readme_file = open('README.md', 'w')
@@ -30,15 +45,15 @@ This is a backup of my Medium blog. [https://medium.com/@thebojda](https://mediu
  
 ''')
 
-parser = TitleExtractor()
 files = os.listdir('posts')
 files.sort()
 for post in files:
     file = open('posts/' + post)
     html = file.read()
     file.close()
+    parser = TitleExtractor()
     parser.feed(html)
     readme_file.write(
-        '[' + parser.get_title() + '](' + 'https://thebojda.github.io/my-medium-blog/posts/' + post + ')\n\n')
+        '[' + parser.get_title() + ' ' + parser.get_subtitle() + '](' + 'https://thebojda.github.io/my-medium-blog/posts/' + post + ')\n\n')
 
 readme_file.close()
